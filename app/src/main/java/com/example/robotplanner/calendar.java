ab9dc100e54da9609a69f9bd2583656080192b78
Dawid Białek <dd.bialek@gmail.com>
2019-05-29 12:40:59 +0200

    Add connection to database

--- app/src/main/java/com/example/robotplanner/calendar.java
+++ app/src/main/java/com/example/robotplanner/calendar.java
@@ -8,10 +8,25 @@ import android.widget.Button;
 
 public class calendar extends AppCompatActivity {
 
+    Button buttonAddItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        buttonAddItem = (Button)findViewById(R.id.btn_add_item);
+        buttonAddItem.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                if(v==buttonAddItem){
+
+                    Intent intent = new Intent(getApplicationContext(),AddItem.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
 
         final Button buttonController = findViewById(R.id.controllerButton);
 
@@ -21,4 +36,6 @@ public class calendar extends AppCompatActivity {
             }
         });
     }
+
+
 }
